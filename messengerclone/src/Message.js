2a134a8824b1e5a23960848f9b2c2bf9ab6577ee
Mary <maryrose.legaspi2018@gmail.com>
2020-11-11 13:44:47 +1000

    Update Message.js

--- messengerclone/src/Message.js
+++ messengerclone/src/Message.js
@@ -1,14 +1,26 @@
+import { Card, CardContent, Typography } from '@material-ui/core';
 import React from 'react';
 // import './Message.css'
 
 function Message({username, text}) {
     return (
-        <div className ='message'>
-            {/* <h4>{username}</h4> */}
+        // <div className ='message'>
+        //     {/* <h4>{username}</h4> */}
             
-            <h2>{username} : {text}</h2>
+        //     <h2>{username} : {text}</h2>
             
-        </div>
+        // </div>
+        <Card >
+            <CardContent>
+                <Typography
+                 color='white'
+                 variant='h5'
+                 component='h2'
+                 >
+                    {username}:  {text}
+                </Typography>
+            </CardContent>
+        </Card>
     )
 }
 
