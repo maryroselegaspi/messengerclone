aaee03dfc3d42819071f9084e46714d1712b9bb4
Mary <maryrose.legaspi2018@gmail.com>
2020-11-11 15:09:01 +1000

    update app.js added timestamp and sort by time

--- messengerclone/src/App.js
+++ messengerclone/src/App.js
@@ -3,7 +3,7 @@ import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import './App.css';
 import Message from './Message';
 import db from './firebase';
-
+import firebase from 'firebase';
 
 
 
@@ -16,13 +16,15 @@ function App() {
   const[username,setUserName] = useState('');
 
   useEffect(() => {
-    db.collection('messages').onSnapshot(snap => {
+    db.collection('messages')
+    .orderBy('timestamp', 'desc')
+    .onSnapshot(snapshot => {
       setMessages(snapshot.docs.map(doc => doc.data()))
     })
     
   }, []);
 
-
+  console.log(messages)
   useEffect(() => {
     //run code here
     //if blank, run code once component loads (no condition, only when page loads)
@@ -35,8 +37,15 @@ function App() {
     // all the logic to send the message
 
     event.preventDefault(); //to prevent refresh everytime enter is hit  
+
+    // to ensure that messages are sorted by time
+    db.collection('messages').add({
+      message: input,
+      username:username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()    
+    })
     setMessages([
-      ...messages, {username:username, text:input}
+      ...messages, {username:username, message:input}
     ]) // spread operator => keep all the messages but append the latest to the end
     
       setInput(''); //to clear the input
